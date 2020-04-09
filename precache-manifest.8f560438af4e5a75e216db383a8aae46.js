9d2bfd8bb08cdac21745d7899ce797a8f837a093
Sebastian Cieplak <sebastiancieplak@gmail.com>
2020-04-10 00:11:26 +0200

    Updates

---
+++ precache-manifest.8f560438af4e5a75e216db383a8aae46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a8d21d57df8a70430ed503a4456758",
+    "url": "/mood-wallpapers/index.html"
+  },
+  {
+    "revision": "e8af305c222bd8480351",
+    "url": "/mood-wallpapers/static/js/2.da806aac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mood-wallpapers/static/js/2.da806aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1593364a7da5a5f911e",
+    "url": "/mood-wallpapers/static/js/main.2887d953.chunk.js"
+  },
+  {
+    "revision": "88e1fa2d0dfdd380f80b",
+    "url": "/mood-wallpapers/static/js/runtime-main.9609304e.js"
+  },
+  {
+    "revision": "7eab74ecdd115ddb395f0cba3c0dff8b",
+    "url": "/mood-wallpapers/static/media/red-hat-display.7eab74ec.woff"
+  },
+  {
+    "revision": "20e4fd7eed6831c1778aa056a32b3244",
+    "url": "/mood-wallpapers/static/media/reem-kufi.20e4fd7e.woff"
+  }
+]);
